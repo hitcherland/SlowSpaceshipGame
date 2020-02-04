71e876a6a42ef7b0edd8cd18084459445e091174
hitcherland <hitcherland@gmail.com>
2020-02-04 15:59:58 +0000

    Initial ship interface implementation

---
+++ Assets/Scripts/AbstractClasses/Exceptions.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class NotImplementedException : System.Exception
+{
+    public NotImplementedException() { }
+}
---
+++ Assets/Scripts/AbstractClasses/interfaces/ShipInterface.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//Todo: implement all the required classes to fully define this interface & base class
+
+public interface ShipInterface
+{
+    int calculateCurrentHealth();
+    void activateModuleMountPoint(int moduleMountPointIndex);
+    //void receiveProjectileHit(ProjectileHitEvent ev);
+    void die();
+    void rotateTowardsPoint(Quaternion rotation, float rotationSpeed);
+    void moveInDirection(Vector3 direction, float power);
+}
+
+public class Ship : MonoBehaviour, ShipInterface
+{
+    //public ShipState state;
+    public GameObject model;
+    public int totalBaseHealth;
+    protected int currentBaseHealth;
+    public float enginePower;
+    public float engineRotationSpeed;
+    public Dictionary<string, int> moduleKeyBindings;
+    //ModuleMountPoint[] moduleMountPoints;
+
+    public virtual int calculateCurrentHealth() { throw new NotImplementedException(); }
+    public virtual void activateModuleMountPoint(int moduleMountPointIndex) { throw new NotImplementedException(); }
+    //private virtual void receiveProjectileHit(ProjectileHitEvent ev) { throw new NotImplementedException(); }
+    public virtual void die() { throw new NotImplementedException(); }
+    public virtual void rotateTowardsPoint(Quaternion rotation, float rotationSpeed) { throw new NotImplementedException(); }
+    public virtual void moveInDirection(Vector3 direction, float power) { throw new NotImplementedException(); }
+
+    private void Start()
+    {
+        Instantiate(model, transform);
+    }
+}
