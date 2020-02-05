138ecaefaca85eb72d6edc960783d06d98b57dcf
Oliver Stiel <olistiel@gmail.com>
2020-02-05 17:56:41 +0000

    Start of Movement Changes

--- Assets/Scripts/Ships/CapitalShip.cs
+++ Assets/Scripts/Ships/CapitalShip.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CapitalShip : Ship
 {
+    // TODO: check for ownership, allow for input if true
+    public float pow = 10;
+    public bool owned = true;
+
     // Start is called before the first frame update
     new void Start()
     {
@@ -13,6 +17,10 @@ public class CapitalShip : Ship
     // Update is called once per frame
     void Update()
     {
-        
+        // TODO: moving forwards
+        Quaternion x_quart = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Time.deltaTime * pow, Vector3.up);
+        Quaternion y_quart = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * Time.deltaTime * pow, Vector3.right);
+
+        transform.rotation = transform.rotation * x_quart * y_quart;
     }
 }
