5804c4652566bb9d772ee98dac81f8ce1244dc42
hitcherland <hitcherland@gmail.com>
2020-02-06 16:28:01 +0000

    Added some visual flair while we're testing

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject goal;
+    public Vector3 relativePoint;
+    public Quaternion relativeRotation;
+    public float lerpSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        relativePoint = goal.transform.InverseTransformPoint(transform.position);
+        relativeRotation = Quaternion.Inverse(goal.transform.rotation) * transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 goalPosition = goal.transform.TransformPoint(relativePoint);
+        transform.position = Vector3.Lerp(transform.position, goalPosition, lerpSpeed * Time.deltaTime);
+        Quaternion goalRotation = goal.transform.rotation * relativeRotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, goalRotation, lerpSpeed * Time.deltaTime);
+    }
+}
