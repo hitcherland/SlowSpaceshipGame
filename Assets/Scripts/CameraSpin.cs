97c27b8a08e73551b012f4d0e9dc8da929a46019
hitcherland <hitcherland@gmail.com>
2020-02-09 17:07:26 +0000

    small adjustments

---
+++ Assets/Scripts/CameraSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour
+{
+    public float speed = 1;
+    private Vector3 spin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spin = Random.onUnitSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(spin * speed * Time.deltaTime);
+    }
+}
